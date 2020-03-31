065d246ad3d50d32cd0a35c34292262bfc8b67d0
Berkkan Katirci <berkkan22@gmail.com>
2020-03-31 15:28:20 +0200

    Updates

---
+++ precache-manifest.929aacdf4fbfed10e1e9733b7bddb0e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89c3048ec0f00cd0a49ae4215e937ee",
+    "url": "/randy/index.html"
+  },
+  {
+    "revision": "be657ad8be727e27f224",
+    "url": "/randy/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "1e06e97a18a2f3f0e0dd",
+    "url": "/randy/static/css/main.81881d6b.chunk.css"
+  },
+  {
+    "revision": "be657ad8be727e27f224",
+    "url": "/randy/static/js/2.a82587c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randy/static/js/2.a82587c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e06e97a18a2f3f0e0dd",
+    "url": "/randy/static/js/main.c1a03990.chunk.js"
+  },
+  {
+    "revision": "5899664dab5cc41edde5",
+    "url": "/randy/static/js/runtime-main.374eb09c.js"
+  },
+  {
+    "revision": "668a1b60a239d372a3e0cda003b376f3",
+    "url": "/randy/static/media/Randy_Profiel.668a1b60.jpg"
+  },
+  {
+    "revision": "aea8acaf3316ce3781be0ed5788c8401",
+    "url": "/randy/static/media/Ronaldo2.aea8acaf.jpg"
+  },
+  {
+    "revision": "885324f9e309e1f6ca5fbff1bfc01668",
+    "url": "/randy/static/media/facebook-f.885324f9.svg"
+  },
+  {
+    "revision": "2b349fa9763a5d43d5950e37d1b9dbfe",
+    "url": "/randy/static/media/instagram.2b349fa9.svg"
+  },
+  {
+    "revision": "c93da7fe23f29dcd3e270474bc297488",
+    "url": "/randy/static/media/snapchat.c93da7fe.svg"
+  }
+]);
